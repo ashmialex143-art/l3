c5484aba63409826537496698d488a41db74f257
ashmialex143-art <ashmialex143@gmail.com>
2026-03-23 16:18:52 +0530

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,11 @@
+a = 25
+print("type of a:", type(a))
+
+b = 1.8
+print("type of b:", type(b))
+
+c = "hello"
+print("type of c:", type(c))
+
+d = True
+print("type of d:", type(d))
---
+++ a2.py
@@ -0,0 +1,13 @@
+name=input("enter your name:")
+print("name:", name , type(name))
+
+age=input("enter your age:")
+print("age:", age , type(age))
+age1=int(age)
+print("age:", age1 , type(age1))
+
+valid=(input("do you have a certificate?(True/False):"))
+print("valid:", valid , type(valid))
+v1=False
+print("valid1:", v1 , type(v1))
+
